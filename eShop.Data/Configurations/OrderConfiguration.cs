490d060912a26cba9c32ec33d30e7716b898440a
Vudang <danglegiavu@gmail.com>
2021-06-14 18:25:51 +0700

    apply identity core

--- eShop.Data/Configurations/OrderConfiguration.cs
+++ eShop.Data/Configurations/OrderConfiguration.cs
@@ -19,7 +19,7 @@ namespace eShop.Data.Configurations
             builder.Property(x => x.ShipAddress).IsRequired().HasMaxLength(200);
             builder.Property(x => x.ShipName).IsRequired().HasMaxLength(200);
             builder.Property(x => x.ShipPhoneNumber).IsRequired().HasMaxLength(200);
-
+            builder.HasOne(x => x.AppUser).WithMany(x => x.Orders).HasForeignKey(x => x.UserId);
 
         }
     }
