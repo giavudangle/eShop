b717119252141353d01c0326d122b7627d9b0ea6
Vu Dang <danglegiavu@gmail.com>
2021-06-16 16:50:32 +0700

    Revert "Revert 9 feature/api layer"

---
+++ eShop.ApiService/Startup.cs
@@ -0,0 +1,62 @@
+using eShop.AppService.Catalog.Products;
+using eShop.AppService.Common;
+using eShop.Data.EF;
+using eShop.Utilities.Constants;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.ApiService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<EShopDBContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
+            // Declare Dependency Injection with Transient
+            services.AddTransient<IStorageService, FileStorageService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddAuthorization();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
